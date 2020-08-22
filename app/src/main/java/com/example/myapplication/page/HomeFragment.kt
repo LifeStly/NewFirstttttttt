9ce93770333039e9c643ddb889a5885e087dbe1a
LifeStly <tattonman001@gmail.com>
2020-08-22 14:19:22 +0700

    bindding

--- app/src/main/java/com/example/myapplication/page/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/page/HomeFragment.kt
@@ -1,17 +1,30 @@
 package com.example.myapplication.page
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentHomeBinding
+
 
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
+            R.layout.fragment_home,container,false)
         setHasOptionsMenu(true)
         return binding.root
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.navigate_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,requireView().findNavController())
+                ||super.onOptionsItemSelected(item)
     }
 }
